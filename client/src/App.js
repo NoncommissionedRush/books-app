f1a3e7163521b1d202fb47c2836b855bd248764e
Viktor Milosavljevic <milosavljevic.viktor@gmail.com>
2020-10-01 11:19:49 +0200

    add useState and loading to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
+import Card from "./Card";
 
 function App() {
-  return <div className="App"></div>;
+  const [isLoading, setLoading] = useState(true);
+  const [allBooks, setAllBooks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await axios.get("/api/books");
+      setAllBooks(resp.data);
+      setLoading(false);
+    })();
+  });
+
+  if (isLoading) {
+    return <div className="loader">Loading</div>;
+  }
+
+  return (
+    <div className="App">
+      <Navbar />
+      {allBooks.map((book, index) => (
+        <Card />
+      ))}
+    </div>
+  );
 }
 
 export default App;
